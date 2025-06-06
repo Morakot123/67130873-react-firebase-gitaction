1131c2205ff6b28782719150f75714d4c70de6ab
Morakot123 <67130873@dpu.ac.th>
2025-06-06 11:39:39 +0700

    Delete src directory

--- src/common.js
+++
@@ -1,9 +0,0 @@
-const exports = {};
-exports.API_URL = "https://iotapi.thaionzon.com/getZipcode";
-exports.options = {
-  headers: {
-    "Content-type": "application/json",
-    Authorization: "k4pGvhg7GNKYznhW",
-  },
-};
-export default exports;
